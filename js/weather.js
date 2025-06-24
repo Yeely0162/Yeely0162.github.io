8123111d16fbb8cab48b939bc86ee2e2e0c83511
yeely01621 <yeely01621@163.com>
2025-06-24 22:39:07 +0800

    Site updated: 2025-06-24 22:39:06

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "79baff",
+    "tmpSize": "16",
+    "cityColor": "FF9900",
+    "citySize": "16",
+    "aqiColor": "79baff",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "0px 0px 0px 0px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "center",
+    "horizontal": "center",
+    "left": "100",
+    "top": "20",
+    "key": "fd331c2718c345ac938cabc831816f5a"
+  }
+}
